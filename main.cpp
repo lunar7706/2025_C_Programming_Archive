b14bce7ce9406ebdf4117a41fbcf3a8874f1d9f2
lunar7706 <qwerhjkl263@gmail.com>
2026-04-11 14:38:45 +0900

    “Add main.cpp for ex0310_1st”
    Organizing GitHub repositories using the Git subtree command

---
+++ main.cpp
@@ -0,0 +1,20 @@
+환영합니다, ex250310_1st Wiki
+
+[NAVER](https://www.naver.com)
+*[[C언어 주요 문법 정리]]
+...
+## 1. 연습 제목 1
+- 메롱1
+- 메롱2
+
+## 2. 연습 제목 2
+* 태그1
+* 태그2
+
+```python
+#include <stdio,h>
+int main()
+{
+ return 0;
+}
+```
